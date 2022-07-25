99e4523cf69b700a3923f644223f38ab8437caaa
jbdev <bordet.julian@gmail.com>
2022-07-25 14:21:42 -0300

    Example of using Ract Context combined with useReducer

---
+++ src/components/MyComponent.js
@@ -0,0 +1,44 @@
+import React, { useContext } from "react";
+import { StoreContext } from "../store/StoreProvider";
+import { types } from "../store/StoreReducer";
+
+const MyComponent = () =>{
+
+    const [store, dispatch] = useContext(StoreContext);
+    const {user, products} = store;
+
+    return (
+        <div>
+            <h1>MyComponent</h1>
+            <h2>User: {user?.name}</h2>
+            <button onClick={() => dispatch({ type: types.authLogout})}>
+                Logout
+            </button>
+
+            <button onClick={() => dispatch({
+                type: types.authLogin,
+                payload: { id: 1, name: 'Luis'}
+            })}>
+                Login
+            </button>
+
+            <h2>Products:</h2>
+            <ul>
+                {products.map(product => (
+                    <li key={product.id}>{product.title}</li>
+                ))}
+                
+            </ul>
+
+            <button onClick={() => dispatch({type: types.productDeleteAll})}>
+                Delete all
+            </button>
+
+            <button onClick={() => dispatch({type: types.productChange})}>
+                Change
+            </button>
+        </div>
+    )
+}
+
+export default MyComponent;
